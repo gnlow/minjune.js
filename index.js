4234457a0753da2473884828c76b1f68e1a28199
EntryDark <entrydark@gmail.com>
2019-07-12 00:15:56 +0900

    Add 'Kid' class

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const boy = require("./boy.js");
 const girl = require("./girl.js");
 
+const nameLength = boy.length + girl.length;
+
 let random = (max) => Math.round((max * Math.random()));
 
-console.log(boy[random(boy.length)]);
+class Kid{
+	constructor(seed){
+		this.seed = seed;
+		this.gender = seed % 2
+		if(!this.gender){ //boy
+			this.name = boy[Math.trunc(seed/2) % boy.length];
+		}else{ //girl
+			this.name = girl[Math.trunc(seed/2) % girl.length];
+		}
+	}
+}
+
+var a = random(nameLength);
+console.log(new Kid(a).name, a);
