de4addc55f56bf47683f1f1d3a865422e175392a
EntryDark <entrydark@gmail.com>
2019-07-13 11:10:00 +0900

    add Kid.age, Kid.grade

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const nameLength = (boy.length + girl.length) * lastname.length * (360 * 7);
 
 const day = 1000 * 60 * 60 * 24;
 
+const grades = [
+	{school: "es", grade: 1},
+	{school: "es", grade: 2},
+	{school: "es", grade: 3},
+	{school: "es", grade: 4},
+	{school: "es", grade: 5},
+	{school: "es", grade: 6},
+	{school: "ms", grade: 1},
+	{school: "ms", grade: 2},
+	{school: "ms", grade: 3},
+];
+
 let random = (max) => Math.round((max * Math.random()));
 
 class Kid{
@@ -21,6 +33,9 @@ class Kid{
 		this.name = this.lastname + this.firstname;
 
 		this.birth = new Date(1103932800000 + day * (seed % (360 * 7)) );
+		var today = new Date();
+		this.age = today.getYear() - this.birth.getYear() + 1;
+		this.grade = grades[this.age-8];
 	}
 }
 
